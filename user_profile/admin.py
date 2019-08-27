369d0de3368e4a8e7bd59212c238b0908c9d76e7
thomas <thomas.adel31@gmail.com>
2019-08-27 14:42:28 +0200

    migrate verfy sms

--- user_profile/admin.py
+++ user_profile/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Profile, Address
+from .models import Profile, Address, SMSVerification
 
 admin.site.register(Profile)
 admin.site.register(Address)
+admin.site.register(SMSVerification)
