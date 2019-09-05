6398dee60cd96407994d7cac02767e64ce8938f3
thomas <thomas.adel31@gmail.com>
2019-09-05 12:38:12 +0200

    make validation in phone verification

--- user_profile/auth_backends.py
+++ user_profile/auth_backends.py
@@ -9,8 +9,10 @@ class EmailBackend(AllowAllUsersModelBackend):
     def authenticate(self, request, username=None, password=None, **kwargs):
         if username is None:
             username = kwargs.get("username")
+        # if User.objects.get(profile__phone_number=username).sms.first().verified == False:
+        #     raise PermissionDenied("you can't login by phone Number")
         try:
-            if username[0] == '+':
+            if username[0] == '+' and User.objects.get(profile__phone_number=username).sms.first().verified == True:
                 user = User.objects.get(profile__phone_number=username)
             else:
                 user = User.objects.get(email=username)
