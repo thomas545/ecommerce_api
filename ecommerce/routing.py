3421d73a4e4311fbc5657cbe3d77875a6346aec1
thomas <thomas.adel31@gmail.com>
2019-10-10 10:39:59 +0200

    channels urls

--- ecommerce/routing.py
+++ ecommerce/routing.py
@@ -10,7 +10,8 @@ application = ProtocolTypeRouter({
     # (http->django views is added by default)
     'websocket': JwtTokenAuthMiddleware(
         URLRouter(
-            chat.routing.websocket_urlpatterns + notifications.routing.websocket_urlpatterns
+            chat.routing.websocket_urlpatterns +
+            notifications.routing.websocket_urlpatterns
         )
     )
 })
