1d09eca52e7a1f2b7b69430a2764784724a855c1
thomas434 <thomas@dokkanz.com>
2020-06-20 13:23:31 +0200

    add model extentions

--- core/models.py
+++ core/models.py
@@ -1,6 +1,19 @@
+import uuid
+import os
 from django.db import models
 
+
 class TimeStampedModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Extensions(models.Model):
+    """ Best practice for lookup field url instead pk or slug """
+    uuid = models.UUIDField(db_index=True, default=uuid.uuid4, editable=False)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
 
