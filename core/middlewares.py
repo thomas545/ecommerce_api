82fa9911280e00750194c6a5c8c61f43be8d78ca
thomas434 <thomas@dokkanz.com>
2020-06-20 13:23:19 +0200

    add seo

--- core/middlewares.py
+++ core/middlewares.py
@@ -1,4 +1,6 @@
 
+import socket
+
 from django.conf import settings
 from django.core.cache import DEFAULT_CACHE_ALIAS, caches
 from django.utils.cache import (
@@ -6,6 +8,33 @@ from django.utils.cache import (
     patch_response_headers,
 )
 from django.utils.deprecation import MiddlewareMixin
+from django.contrib.auth.models import AnonymousUser
+from django.contrib.auth.models import User
+
+
+class GooglebotMiddleware(object):
+    """
+    Middleware to automatically log in the Googlebot with the user account 'googlebot'
+    """
+    def process_request(self, request):
+        request.is_googlebot = False # Assume false, until proven
+        if request.user == AnonymousUser():
+            if request.META.get('HTTP_USER_AGENT'):
+                if 'Googlebot' in request.META['HTTP_USER_AGENT']:
+                    try:
+                        remote_ip = request.META['REMOTE_ADDR']
+                        hostname = socket.gethostbyaddr(remote_ip)[0]
+
+                        if hostname.endswith('googlebot.com'):
+                            request.user, created = User.objects.get_or_create(username='googlebot') # login our googlebot user :)
+                            request.is_googlebot = True
+                        else:
+                            # FAKE googlebot!!!!
+                            request.is_googlebot = False
+
+                    except Exception:
+                        pass # Don't bring down the site
+        return None
 
 
 class UpdateCacheMiddleware(MiddlewareMixin):
--- ecommerce/settings/production.py
+++ ecommerce/settings/production.py
@@ -2,3 +2,17 @@ from .staging import *
 
 
 DEBUG = False
+
+
+"""
+Finally, create a googlebot user. 
+This account will be used when Googlebot is automatically logged in. 
+If you don't create an account, then one will be created automatically.
+
+NB If you wish to disable the Google Cache feature, create a robots.txt with Noarchive . Eg:
+
+User-agent: *
+Disallow:
+Noarchive: /restricted-content/
+"""
+MIDDLEWARE += ['core.middlewares.GooglebotMiddleware', ]
