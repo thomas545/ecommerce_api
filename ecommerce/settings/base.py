65621603f227100572ff97ae1c1eada335e460c9
Thomas Adel <43293944+thomas545@users.noreply.github.com>
2020-07-04 12:40:12 +0200

    Update base.py

--- ecommerce/settings/base.py
+++ ecommerce/settings/base.py
@@ -291,6 +291,7 @@ TWILIO_FROM_NUMBER = config('TWILIO_FROM')
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, "static"),]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
